fad9b625f9eb4f0c61f4b056bb803214b77f8714
EC2 Default User <ec2-user@ip-172-31-23-55.ap-northeast-1.compute.internal>
2020-12-28 13:22:07 +0000

    resources :foods, except: :showを修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
+  resources :foods, except: :show
   root 'home#index'
   get 'home/baby_food' => 'home#baby_food'
   get 'home/about_app' => 'home#about#app'
-  resources :foods, except: :show
+  
 end
