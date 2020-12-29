fd4392ea473b5d0452ed518c2abbde9866ba073e
EC2 Default User <ec2-user@ip-172-31-23-55.ap-northeast-1.compute.internal>
2020-12-29 01:22:36 +0000

    ルーティングを編集

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :foods, except: :show
   root 'home#index'
   get 'home/baby_food' => 'home#baby_food'
-  get 'home/about_app' => 'home#about#app'
+  get 'home/about_app' => 'home#about_app'
+  
   
 end
